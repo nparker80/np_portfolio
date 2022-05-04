e358bb2724430f1f48fbd779c81fec533ef1aca7
Najla Parker <najla08@gmail.com>
2022-05-04 17:14:17 -0600

    REACT HOC for wrapper

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
-import './About.scss';
+import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
+import './About.scss';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
@@ -39,4 +39,4 @@ const About = () => {
   );
 };
 
-export default About
+export default AppWrap(About, 'about');
