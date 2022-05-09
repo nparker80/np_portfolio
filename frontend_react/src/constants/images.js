3fce61963c06d851d65dcac9423e014c48568398
Najla Parker <najla08@gmail.com>
2022-05-09 14:29:09 -0600

    added skills

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -9,6 +9,7 @@ import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
+import mongodb from '../assets/mongodb.png'
 import mu5 from '../assets/mu5.png';
 import node from '../assets/node.png';
 import python from '../assets/python.png';
@@ -51,6 +52,7 @@ export default {
   graphql,
   html,
   javascript,
+  mongodb,
   mu5,
   node,
   python,
