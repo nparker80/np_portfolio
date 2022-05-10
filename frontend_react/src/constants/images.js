cc18753f857f2fdafb334b17e66e4e0a3203e862
Najla Parker <najla08@gmail.com>
2022-05-09 21:28:53 -0600

    added work experience. fix bug in the future cannot add NV

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,26 +1,23 @@
+import bootstrap from '../assets/bootstrap.png';
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
 import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
-import figma from '../assets/figma.png';
-import flutter from '../assets/flutter.png';
+import express from '../assets/express.png';
 import git from '../assets/git.png';
 import graphql from '../assets/graphql.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
 import mongodb from '../assets/mongodb.png'
-import mu5 from '../assets/mu5.png';
+import MySQL from '../assets/MySQL.png'
 import node from '../assets/node.png';
-import python from '../assets/python.png';
 import react from '../assets/react.png';
-import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
-import typescript from '../assets/typescript.png';
-import vue from '../assets/vue.png';
 
 import codey from '../assets/codey-buddy.png';
 import e_commerce from '../assets/E-commerce_sql.png';
+import gymbunny from '../assets/gymbunny.png';
+import techblog from '../assets/techblog.png';
 
 
 import about01 from '../assets/about01.png';
@@ -32,37 +29,28 @@ import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
-import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
 
 export default {
   email,
   mobile,
   api,
-  cpp,
+  bootstrap,
   css,
-  figma,
-  flutter,
+  express,
   git,
   graphql,
   html,
   javascript,
   mongodb,
-  mu5,
+  MySQL,
   node,
-  python,
   react,
-  redux,
   sass,
-  typescript,
-  vue,
   codey,
   e_commerce,
+  gymbunny,
+  techblog,
   about01,
   about02,
   about03,
@@ -70,11 +58,5 @@ export default {
   profile,
   circle,
   logo,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
   skype,
-  spotify,
 };
--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
-import './Testimonial.scss'
+// import './Testimonial.scss'
 
-const Testimonial = () => {
-  return (
-    <div>Testimonial</div>
-  )
-}
+// const Testimonial = () => {
+//   return (
+//     <div>Testimonial</div>
+//   )
+// }
 
-export default Testimonial
+// export default Testimonial
